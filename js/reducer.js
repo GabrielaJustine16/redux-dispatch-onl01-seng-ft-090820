65033ec42eb6249fc789f7c5052793128b8cf2da
Gabriela Johnson <johnsongabriela12@gmail.com>
2021-02-06 18:44:59 -0800

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -6,6 +6,32 @@ function changeState(state, action){
       return state;
   }
 }
+let state = {count: 0}
+let action = {type: 'INCREASE_COUNT'}
+
+function changeState(state, action){
+  switch (action.type) {
+    case 'INCREASE_COUNT':
+      return {count: state.count + 1}
+    default:
+      return state;
+  }
+}
+
+// changeState(state, action)
+
+
+function render(){
+  document.body.textContent= state.count
+}
+
+
+function dispatch(action) { 
+  state = changeState(state, action)
+  render()
+}
+
+render()
 
 let state = {count: 0}
 let action = {type: 'INCREASE_COUNT'}
